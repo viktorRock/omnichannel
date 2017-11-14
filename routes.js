6235c3a4d8d5f9849a75cd8f64efa38dca111a17
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2017-11-13 22:58:04 -0200

    ajustando portas Socket

--- routes.js
+++ routes.js
@@ -51,9 +51,8 @@ router.post('/api/messages', function(req, res, next) {
   // console.log("req.body.type = ");
   // console.log(req.body);
   let msg = parseBotFramToMessage(req.body);
-  console.log(req.body);
-  console.log(req.body.address);
-  console.log(req.body.address.bot);
+  // console.log(req.body);
+  console.log(req);
   if(req.body.type == BOTFRAMEWORK_MESSAGE){
     results.messages.push(msg);
     chat_client.emit(msg);
